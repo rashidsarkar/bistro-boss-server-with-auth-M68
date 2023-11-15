b1839a83206071f3760d8225a9646e641b830d5d
rashidsarkar <rashidrock558@gmail.com>
2023-11-15 20:58:42 +0600

    user info save on DB

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+//midleWare
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ydmxw3q.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    const userCollection = client.db("bistroDB").collection("users");
+    const menuCollection = client.db("bistroDB").collection("menu");
+    const reviewsCollection = client.db("bistroDB").collection("reviews");
+    const cartCollection = client.db("bistroDB").collection("cart");
+    // await client.connect();
+
+    //NOTE -  User API
+    app.post("/api/users", async (req, res) => {
+      const user = req.body;
+      //NOTE -  insert email if user doesn't exists
+      //NOTE -  you can do this many ways (1. email unique, 2. upsert , 3. simple checking)
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      console.log(existingUser);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // card collection
+    app.post("/api/addTocarts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+    app.get("/api/allCartsItem", async (req, res) => {
+      const emailQuery = req.query.email;
+      const query = { email: emailQuery };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.delete("/api/cartsDelete/:id", async (req, res) => {
+      const idQuery = req.params.id;
+      const query = { _id: new ObjectId(idQuery) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Boss IS Running");
+});
+app.listen(port, () => {
+  console.log(`server is Running on ${port}`);
+});
